8454ba97d9d9ce9dd04435e71421ef4c9aedaf8b
Aryanmittal23 <mittalaryan932@gmail.com>
2024-04-30 02:36:50 +0530

    ml

--- day4.py
+++ day4.py
@@ -18,7 +18,18 @@ print(list)
 
 print([1,2,3,4]) # it is 1D list and homogenous list
 print([1,2,3,[4,5]]) # 2D
-print([1,1,[2,2,2,[3,3,4]]]) # 3D"""
+print([1,1,[2,2,2,[3,3,4]]]) # 3D
 print(list('hello'))
 
+
+List Comprehension
+List Comprehension provides a concise way of creating lists.
+
+newlist = [expression for item in iterable if condition == True]image.png
+
+Advantages of List Comprehension
+
+More time-efficient and space-efficient than loops.
+Require fewer lines of code.
+Transforms iterative statement into a formula."""
 
